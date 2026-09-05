2718256306e4882ad95ada0dd2e2b294604d4119
freyazhao88-netizen <freyazhao88@gmail.com>
2026-09-05 12:07:33 -0700

    Update: server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const materialRoutes = require("./src/routes/materials");
 const stageRoutes = require("./src/routes/stages");
 const quoteRoutes = require("./src/routes/quotes");
 const changeOrderRoutes = require("./src/routes/changeOrders");
+const paymentRoutes = require("./src/routes/payments");
 
 const app = express();
 const PORT = process.env.PORT || 4173;
@@ -60,6 +61,7 @@ app.use("/api", materialRoutes);
 app.use("/api", stageRoutes);
 app.use("/api", quoteRoutes);
 app.use("/api", changeOrderRoutes);
+app.use("/api", paymentRoutes);
 
 app.use("/api", (err, req, res, next) => {
   console.error(err);
